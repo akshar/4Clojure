41ebaaea229a6e3f1bd8ea66d2b630e3b57d4fea
Akshar Dave <akshar314@gmail.com>
2017-03-02 23:35:11 +0530

    first 5 problems

---
+++ introlist.clj
@@ -0,0 +1,3 @@
+(= (list :a :b :c) '(:a :b :c))
+
+;; list can be created by (list 1 2 3)
---
+++ introvector.clj
@@ -0,0 +1,5 @@
+(= [:a :b :c] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))
+
+
+
+;;(vector :a :b :c) => [:a :b :c]
